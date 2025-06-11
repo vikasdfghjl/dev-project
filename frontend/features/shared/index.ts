16022f295561ad219f969e1cb7b7f5bf5266d3e7
Vikas <shyvikas63@gmail.com>
2025-06-12 00:59:01 +0530

    fix: resolve shared icons import path issues for Docker build

--- frontend/features/shared/index.ts
+++ frontend/features/shared/index.ts
@@ -6,20 +6,4 @@ export { Modal } from "./components/Modal";
 export * from "./ui";
 
 // Icons
-export * from "./icons/BookIcon";
-export * from "./icons/BookOpenIcon";
-export * from "./icons/CardViewIcon";
-export * from "./icons/ChevronDoubleLeftIcon";
-export * from "./icons/ChevronDoubleRightIcon";
-export * from "./icons/ChevronDownIcon";
-export * from "./icons/ChevronLeftIcon";
-export * from "./icons/ChevronRightIcon";
-export * from "./icons/EditIcon";
-export * from "./icons/ExternalLinkIcon";
-export * from "./icons/FolderIcon";
-export * from "./icons/GearIcon";
-export * from "./icons/ListViewIcon";
-export * from "./icons/PlusIcon";
-export * from "./icons/RefreshIcon";
-export * from "./icons/RssIcon";
-export * from "./icons/TrashIcon";
+export * from "./icons";
