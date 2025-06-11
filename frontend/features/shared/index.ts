e7e46d6c51e6837de6422acbf0031ab720d6a521
Vikas <shyvikas63@gmail.com>
2025-06-12 01:00:50 +0530

    fix: use individual icon exports for better Docker compatibility

--- frontend/features/shared/index.ts
+++ frontend/features/shared/index.ts
@@ -5,5 +5,21 @@ export { Modal } from "./components/Modal";
 // UI components
 export * from "./ui";
 
-// Icons
-export * from "./icons";
+// Icons - individual exports for better compatibility
+export { BookIcon } from "./icons/BookIcon";
+export { BookOpenIcon } from "./icons/BookOpenIcon";
+export { CardViewIcon } from "./icons/CardViewIcon";
+export { ChevronDoubleLeftIcon } from "./icons/ChevronDoubleLeftIcon";
+export { ChevronDoubleRightIcon } from "./icons/ChevronDoubleRightIcon";
+export { ChevronDownIcon } from "./icons/ChevronDownIcon";
+export { ChevronLeftIcon } from "./icons/ChevronLeftIcon";
+export { ChevronRightIcon } from "./icons/ChevronRightIcon";
+export { EditIcon } from "./icons/EditIcon";
+export { ExternalLinkIcon } from "./icons/ExternalLinkIcon";
+export { FolderIcon } from "./icons/FolderIcon";
+export { GearIcon } from "./icons/GearIcon";
+export { ListViewIcon } from "./icons/ListViewIcon";
+export { PlusIcon } from "./icons/PlusIcon";
+export { RefreshIcon } from "./icons/RefreshIcon";
+export { RssIcon } from "./icons/RssIcon";
+export { TrashIcon } from "./icons/TrashIcon";
